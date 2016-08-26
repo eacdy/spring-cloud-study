3381006198bcae57484717e3238b2322990d3868
里目 <limu.zl@alibaba-inc.com>
2016-08-26 18:16:03 +0800

    discovery client user microservice not finished

---
+++ discovery/discovery-client-user/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user (id, name, password) values (1,'Tom','123456');
+insert into user (id, name, password) values (2,'Jerry', 'pwd');
+insert into user (id, name, password) values (3,'Reno', 'abc');
+insert into user (id, name, password) values (4,'Josh', 'password');
