d4a339f25b78f2fdf7ec4f99185d770a6cbe1fed
eacdy0000@126.com <eacdy0000@126.com>
2016-08-26 22:01:57 +0800

    discovery client user service

--- discovery/discovery-client-user/src/main/java/com/itmuch/cloud/scaffold/UserController.java
+++ discovery/discovery-client-user/src/main/java/com/itmuch/cloud/scaffold/UserController.java
@@ -1,6 +1,7 @@
 package com.itmuch.cloud.scaffold;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -8,15 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 作用：测试服务实例的相关内容
+ * @author eacdy
+ */
 @RestController
 public class UserController {
-	Logger logger = org.slf4j.LoggerFactory.getLogger(UserController.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 	@Autowired
-	private DiscoveryClient client;
+	private DiscoveryClient discoveryClient;
 
+	/**
+	 * 注：@GetMapping("/{id}")是spring 4.3的新注解等价于：
+	 * @RequestMapping(value = "/id", method = RequestMethod.GET)
+	 * 类似的注解还有@PostMapping等等
+	 * @param id
+	 * @return 本地服务实例的信息
+	 */
 	@GetMapping("/{id}")
-	public void queryById(@PathVariable Integer id) {
-		ServiceInstance localServiceInstance = this.client.getLocalServiceInstance();
-		this.logger.info(localServiceInstance.toString());
+	public ServiceInstance queryById(@PathVariable Integer id) {
+		LOGGER.debug("接收的参数：{}", id);
+		ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+		return localServiceInstance;
 	}
 }
--- discovery/discovery-client-user/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user (id, name, password) values (1,'Tom','123456');
-insert into user (id, name, password) values (2,'Jerry', 'pwd');
-insert into user (id, name, password) values (3,'Reno', 'abc');
-insert into user (id, name, password) values (4,'Josh', 'password');
