3bde62270f96b15f87393eebed8ee482ae0614f3
里目 <limu.zl@alibaba-inc.com>
2016-08-29 17:50:10 +0800

    eureka user-service

---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/study/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.itmuch.cloud.study.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.itmuch.cloud.study.domain.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
