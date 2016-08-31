c9356dc0dc74929cdce90c0c056a8a706142b404
里目 <limu.zl@alibaba-inc.com>
2016-09-01 00:45:48 +0800

    1. 修改feign代码 2. 添加hystrix-dashboard项目

---
+++ microservice-consumer-movie-feign-with-hystrix-stream/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignHystrixClient.java
@@ -0,0 +1,45 @@
+package com.itmuch.cloud.study.user.feign;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itmuch.cloud.study.user.entity.User;
+import com.itmuch.cloud.study.user.feign.UserFeignHystrixClient.HystrixClientFallback;
+
+/**
+ * 使用@FeignClient注解的fallback属性，指定fallback类
+ * @author eacdy
+ */
+@FeignClient(name = "microservice-provider-user", fallback = HystrixClientFallback.class)
+public interface UserFeignHystrixClient {
+	@RequestMapping("/{id}")
+	public User findById(@RequestParam("id") Long id);
+
+	/**
+	 * 这边采取了和Spring Cloud官方文档相同的做法，将fallback类作为内部类放入Feign的接口中，当然也可以单独写一个fallback类。
+	 * @author eacdy
+	 */
+	@Component
+	static class HystrixClientFallback implements UserFeignHystrixClient {
+		private static final Logger LOGGER = LoggerFactory.getLogger(HystrixClientFallback.class);
+
+		/**
+		 * hystrix fallback方法
+		 * @param id id
+		 * @return 默认的用户
+		 */
+		@Override
+		public User findById(Long id) {
+			LOGGER.info("异常发生，进入fallback方法，接收的参数：id = {}", id);
+			User user = new User();
+			user.setId(-1L);
+			user.setUsername("default username");
+			user.setAge(0);
+			return user;
+		}
+	}
+}
