d0a2550110fe72bd47d08f69468d594822467e63
里目 <limu.zl@alibaba-inc.com>
2016-08-31 20:41:19 +0800

    hystrix demo

--- microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/study/user/controller/RibbonHystrixController.java
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/study/user/controller/RibbonHystrixController.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itmuch.cloud.study.user.entity.User;
-import com.itmuch.cloud.study.user.feign.UserFeignClient;
+import com.itmuch.cloud.study.user.service.RibbonHystrixService;
 
 @RestController
-public class TestFeignController {
+public class RibbonHystrixController {
 	@Autowired
-	private UserFeignClient userClient;
+	private RibbonHystrixService ribbonHystrixService;
 
-	@GetMapping("feign/{id}")
-	public User test(@PathVariable Long id) {
-		User user = this.userClient.findById(id);
-		return user;
+	@GetMapping("/ribbon/{id}")
+	public User findById(@PathVariable Long id) {
+		return this.ribbonHystrixService.findById(id);
 	}
 }
--- microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/service/TestRibbonService.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/service/TestRibbonService.java
@@ -2,7 +2,6 @@ package com.itmuch.cloud.study.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import com.itmuch.cloud.study.user.entity.User;
@@ -12,7 +11,7 @@ public class TestRibbonService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	public User findById(@PathVariable Long id) {
+	public User findById(Long id) {
 		// http://服务提供者的serviceId/url
 		return this.restTemplate.getForObject("http://microservice-provider-user/1", User.class);
 	}
