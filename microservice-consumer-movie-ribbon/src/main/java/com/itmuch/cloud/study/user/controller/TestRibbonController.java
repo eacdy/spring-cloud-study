09154be3b699b5fb1c6db708c5cd23f5b82937ae
里目 <limu.zl@alibaba-inc.com>
2016-08-31 19:11:29 +0800

    change the code of ribbon demo

--- microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/controller/TestRibbonController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/controller/TestRibbonController.java
@@ -4,18 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.itmuch.cloud.study.user.entity.User;
+import com.itmuch.cloud.study.user.service.TestRibbonService;
 
 @RestController
 public class TestRibbonController {
 	@Autowired
-	private RestTemplate restTemplate;
+	private TestRibbonService testRibbonService;
 
 	@GetMapping("/ribbon/{id}")
 	public User findById(@PathVariable Long id) {
-		// http://服务提供者的serviceId/url
-		return this.restTemplate.getForObject("http://microservice-provider-user/1", User.class);
+		return this.testRibbonService.findById(id);
 	}
 }
