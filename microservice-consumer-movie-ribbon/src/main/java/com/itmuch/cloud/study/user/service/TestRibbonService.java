bb389aa401433dfb4c295977cf97caa19533b0df
eacdy0000@126.com <eacdy0000@126.com>
2016-08-31 19:11:29 +0800

    change the code of ribbon demo

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/service/TestRibbonService.java
@@ -0,0 +1,19 @@
+package com.itmuch.cloud.study.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.study.user.entity.User;
+
+@Service
+public class TestRibbonService {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public User findById(@PathVariable Long id) {
+		// http://服务提供者的serviceId/url
+		return this.restTemplate.getForObject("http://microservice-provider-user/1", User.class);
+	}
+}
