3703c583ab00188820711a01053dde9f49145a9d
eacdy0000@126.com <eacdy0000@126.com>
2016-09-02 20:49:32 +0800

    完善注释等。

--- microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/study/hystrixdashboard/HystrixDashboardApplication.java
+++ microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/study/hystrixdashboard/HystrixDashboardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 
 /**
  * 测试步骤:
- * 1. 访问http://localhost:8030/hystrix.stream 可以查看dashboard
+ * 1. 访问http://localhost:8030/hystrix.stream 可以查看Dashboard
  * 2. 在上面的输入框填入: http://想监控的服务:端口/hystrix.stream进行测试
  * 注意：首先要先调用一下想监控的服务的API，否则将会显示一个空的图表.
  * @author eacdy
--- microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/turbine/TurbineApplication.java
+++ microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/turbine/TurbineApplication.java
@@ -1,9 +1,13 @@
-package com.itmuch.cloud.study.hystrixdashboard;
+package com.itmuch.cloud.study.turbine;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * 通过@EnableTurbine接口，激活对Turbine的支持。
+ * @author eacdy
+ */
 @SpringBootApplication
 @EnableTurbine
 public class TurbineApplication {
