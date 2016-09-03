2c5426b18b9b9858328bc3acb568185df378fb18
里目 <limu.zl@alibaba-inc.com>
2016-09-03 16:06:26 +0800

    配置中心

---
+++ microservice-config-server/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -0,0 +1,30 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 通过@EnableConfigServer注解激活配置服务.
+ * 说明：
+ * 在application.yml中有个git.uri的配置，目前配置的是https://github.com/eacdy/spring-cloud-study/
+ * 获取git上的资源信息遵循如下规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ * 
+ * 例如本例：可使用以下路径来访问microservice-config-client-dev.properties：
+ * http://localhost:8040/microservice-config-client-dev.properties
+ * http://localhost:8040/microservice-config-client/dev
+ * ...
+ * @author eacdy
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
