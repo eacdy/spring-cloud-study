d5bb454089e01af13d9c147374bd3c19288b802e
leo <leo@vmeigo.com>
2016-09-07 16:51:02 +0800

    api-gateway集成spring-cloud-securty和spring-cloud-oauth2,实现授权和鉴权

--- microservice-api-gateway/src/main/java/com/itmuch/cloud/study/ZuulApiGatewayApplication.java
+++ microservice-api-gateway/src/main/java/com/itmuch/cloud/study/ZuulApiGatewayApplication.java
@@ -3,6 +3,8 @@ package com.itmuch.cloud.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * 使用@EnableZuulProxy注解激活zuul，同时跟进入该注解可以看到该注解整合了@EnableCircuitBreaker、@EnableDiscoveryClient，是个组合注解，目的是简化配置。
@@ -10,6 +12,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableResourceServer
+@EnableAuthorizationServer
 public class ZuulApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayApplication.class, args);
