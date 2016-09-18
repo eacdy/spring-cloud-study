961ddc1d994c89d0ae47eb3e22524e64e80c0618
里目 <limu.zl@alibaba-inc.com>
2016-09-18 22:29:13 +0800

    Config Server 与 Eureka联合使用

---
+++ microservice-config-server-eureka/src/main/java/com/itmuch/cloud/study/ConfigServerEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerEurekaApplication.class, args);
+    }
+}
