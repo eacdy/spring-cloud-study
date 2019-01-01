bcb6929e20aeea6edc0c10c9b6cdb286b18d14a2
里目 <limu.zl@alibaba-inc.com>
2019-01-01 21:54:40 +0800

    05-服务注册与服务发现-Eureka

---
+++ 2018-Finchley/microservice-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhouli
+ */
+@SpringBootApplication
+public class ConsumerMovieApplication {
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieApplication.class, args);
+  }
+}
