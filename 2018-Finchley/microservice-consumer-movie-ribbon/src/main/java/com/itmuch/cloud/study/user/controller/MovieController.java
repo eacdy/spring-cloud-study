673bd26bb90c80bc5f3d5e0ed8c1e3d1d8afa1b7
里目 <limu.zl@alibaba-inc.com>
2019-01-05 22:03:35 +0800

    Ribbon入门

---
+++ 2018-Finchley/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,31 @@
+package com.itmuch.cloud.study.user.controller;
+
+import com.itmuch.cloud.study.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhouli
+ */
+@RequestMapping("/movies")
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @GetMapping("/users/{id}")
+  public User findById(@PathVariable Long id) {
+    // 这里用到了RestTemplate的占位符能力
+    User user = this.restTemplate.getForObject(
+      "http://microservice-provider-user/users/{id}",
+      User.class,
+      id
+    );
+    // ...电影微服务的业务...
+    return user;
+  }
+}
