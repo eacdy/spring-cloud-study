874db191e645c95da18ec6c96ed7b9ccdd1ffe44
里目 <limu.zl@alibaba-inc.com>
2019-06-20 17:04:50 +0800

    让ribbon支持nacos权重的三种方式`

--- 2019-Spring-Cloud-Alibaba/microservice-consumer-movie-ribbon-rule-with-nacos/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ 2019-Spring-Cloud-Alibaba/microservice-consumer-movie-ribbon-rule-with-nacos/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -1,9 +1,11 @@
 package com.itmuch.cloud.study;
 
+import config.NacosRibbonClientExtendConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-//@RibbonClients(defaultConfiguration = NacosRibbonClientExtendConfiguration.class)
+@RibbonClients(defaultConfiguration = NacosRibbonClientExtendConfiguration.class)
 public class ConsumerMovieApplication {
   @Bean
   @LoadBalanced
