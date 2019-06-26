7c091117b5a50fa9d6877e0299612a7807f45719
里目 <limu.zl@alibaba-inc.com>
2019-06-26 20:10:49 +0800

    Spring Cloud Alibaba扩展Ribbon支持版本管理

---
+++ 2019-Spring-Cloud-Alibaba/microservice-consumer-movie-ribbon-rule-with-nacos-2/src/main/java/com/itmuch/cloud/study/ribbon/NacosFinalRule.java
@@ -0,0 +1,93 @@
+package com.itmuch.cloud.study.ribbon;
+
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.google.common.collect.Lists;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.DynamicServerListLoadBalancer;
+import com.netflix.loadbalancer.Server;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.alibaba.nacos.NacosDiscoveryProperties;
+import org.springframework.cloud.alibaba.nacos.ribbon.NacosServer;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * 复杂均衡规则：
+ * 1. 优先选取同集群下的实例
+ * 2. 元数据匹配
+ * 3. 使用nacos权重
+ *
+ * @author limu.zl
+ */
+@Slf4j
+public class NacosFinalRule extends AbstractLoadBalancerRule {
+    @Autowired
+    private NacosDiscoveryProperties discoveryProperties;
+    @Autowired
+    private NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    @Override
+    public Server choose(Object key) {
+        // 负载均衡规则：优先选择同集群下，符合metadata的实例
+        // 如果没有，就选择所有集群下，符合metadata的实例
+
+        // 1. 查询所有实例 A
+        // 2. 筛选元数据匹配的实例 B
+        // 3. 筛选出同cluster下元数据匹配的实例 C
+        // 4. 如果C为空，就用B
+        // 5. 随机选择实例
+        try {
+            String clusterName = this.nacosDiscoveryProperties.getClusterName();
+            String targetVersion = this.nacosDiscoveryProperties.getMetadata().get("target-version");
+
+            DynamicServerListLoadBalancer loadBalancer = (DynamicServerListLoadBalancer) getLoadBalancer();
+            String name = loadBalancer.getName();
+
+            NamingService namingService = discoveryProperties.namingServiceInstance();
+
+            // 所有实例
+            List<Instance> instances = namingService.selectInstances(name, true);
+
+            List<Instance> metadataMatchInstances = Lists.newArrayList();
+            // 如果配置了版本映射，那么只调用元数据匹配的实例
+            if (StringUtils.isNotBlank(targetVersion)) {
+                metadataMatchInstances = instances.stream()
+                        .filter(instance -> Objects.equals(targetVersion, instance.getMetadata().get("version")))
+                        .collect(Collectors.toList());
+                if (CollectionUtils.isEmpty(metadataMatchInstances)) {
+                    log.warn("未找到元数据匹配的目标实例！请检查配置。targetVersion = {}, instance = {}", targetVersion, instances);
+                    return null;
+                }
+            }
+
+            List<Instance> clusterMetadataMatchInstances = metadataMatchInstances;
+            // 如果配置了集群名称，需筛选同集群下元数据匹配的实例
+            if (StringUtils.isNotBlank(clusterName)) {
+                clusterMetadataMatchInstances = metadataMatchInstances.stream()
+                        .filter(instance -> Objects.equals(clusterName, instance.getClusterName()))
+                        .collect(Collectors.toList());
+                if (CollectionUtils.isEmpty(clusterMetadataMatchInstances)) {
+                    clusterMetadataMatchInstances = metadataMatchInstances;
+                    log.warn("发生跨集群调用。clusterName = {}, targetVersion = {}, clusterMetadataMatchInstances = {}", clusterName, targetVersion, clusterMetadataMatchInstances);
+                }
+            }
+
+            Instance instance = ExtendBalancer.getHostByRandomWeight2(clusterMetadataMatchInstances);
+            return new NacosServer(instance);
+        } catch (Exception e) {
+            log.warn("发生异常", e);
+            return null;
+        }
+    }
+
+    @Override
+    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+    }
+}
